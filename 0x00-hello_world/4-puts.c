ede2199af47f6fa48ca6c6d7154ea6305108f9e2
Atoyebi Mudasiry <atyemi77@hotmail.com>
2022-10-14 03:18:04 +0000

    My first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 /* Print programming is like building a multilingual puzzle*/
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 
 {
-	puts( "\"Programming is like building a multilingual puzzle" );
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
