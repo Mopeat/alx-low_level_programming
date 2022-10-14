fd3ef67d92a7a9c216d667ee31596ef8220ba12b
Atoyebi Mudasiry <atyemi77@hotmail.com>
2022-10-14 19:11:01 +0000

    Adding last digit code

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int n;
+	char last[] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%s %d is %d and is ", last, n, n % 10);
+		if (n % 10 > 5)
+		{
+		printf("greater than 5/n");
+		}
+		else if (n % 10 == 0)
+		{
+		printf("0\n");
+		}
+		else
+		if (n % 10 < 5)
+		{
+		printf("less than 5 but not 0/n");
+		}
+		return (0);
+}
