a28870b3fe02d34f7a5f11f1389cbebc226b6b97
Atoyebi Mudasiry <atyemi77@hotmail.com>
2022-10-14 05:42:56 +0000

    write to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 	int main(void)
 {
-	fwrite(stderr, RED "[and that piece of art is useful\" - Dora Korpar, 2015-10-19]");
+	fwrite(stderr, "[and that piece of art is useful - Dora Korpar, 2015-10-19]");
 	return (0);
 }
