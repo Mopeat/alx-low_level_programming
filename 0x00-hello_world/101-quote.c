a2302f043ceb114aba1d408a325873bbc1b929b3
Atoyebi Mudasiry <atyemi77@hotmail.com>
2022-10-14 05:38:06 +0000

    write to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * REturn: Always 0 (success) 
+ */
+
+	int main(void)
+{
+	fwrite(stderr, RED "[and that piece of art is useful\" - Dora Korpar, 2015-10-19]");
+	return (0);
+}
