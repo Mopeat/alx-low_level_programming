6f9d2056a495b59c398d5465e18d9cd4b14c87df
Atoyebi Mudasiru <atyemi77@hotmail.com>
2022-10-15 11:08:49 +0100

    Display lower case alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,17 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
-{	
+{
 	char i;
+
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
-	for(i = 'a'; i <= 'z'; i++);
-	putchar(i);
+		putchar(i);
 	}
 	putchar('\n');
+
 	return (0);
 }
