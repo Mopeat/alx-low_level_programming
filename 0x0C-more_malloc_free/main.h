a0dbf5d3f79338c7b29d5a722f298038d64c9ab0
Atoyebi Mudasiry <atyemi77@hotmail.com>
2022-11-19 06:16:53 +0000

    update main.h file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,6 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
 int main(int argc, char *argv[]);
+int _putchar(char c);
 
 #endif
