ad6b4d6e2e1afe0997da0b8bb67db7cefdd4789d
Atoyebi Mudasiry <atyemi77@hotmail.com>
2022-11-22 12:21:04 +0000

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef POINTER_H
-#define POINTER_H
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
